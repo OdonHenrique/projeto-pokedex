49e0f91ebcb95f272488e7719c6d6beceeb6d373
Odon Henrique <84817708+OdonHenrique@users.noreply.github.com>
2022-05-05 10:14:30 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,97 +1,21 @@
-const nav = document.querySelector('#header nav')
-const toggle = document.querySelectorAll('nav .toggle')
-const menu = document.querySelectorAll('nav ul li a')
+const listSelect = document.querySelectorAll('.pokemon')
+const cardSelect = document.querySelectorAll('.card')
 
-for (const element of toggle) {
-  element.addEventListener('click', function () {
-    nav.classList.toggle('show')
-    // Caso não tenha a class show, ela será adicionada
-    // Caso tenha a class show, ela será removida
-  })
-}
+listSelect.forEach(pokemon => {
+    pokemon.addEventListener('click', () => {
+        const cardOpen = document.querySelector('.open')
+        cardOpen.classList.remove('open')
+       
+        const pokemonSelectID = pokemon.attributes.id.value
+        const pokemonCardSelecId = 'card-' + pokemonSelectID
 
-for (const links of menu) {
-  links.addEventListener('click', function () {
-    nav.classList.remove('show')
-    // Vai remover a class show independende de ter ela ou não
-  })
-}
+        const pokemonCardSelect = document.getElementById(pokemonCardSelecId)
+        pokemonCardSelect.classList.add('open')
 
-// Colocar sombra no header da página ao dar scroll
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+        const pokemonListSelect = document.querySelector('.select')
+        pokemonListSelect.classList.remove('select')
 
-window.addEventListener('scroll', function () {
-  if (window.scrollY >= navHeight) {
-    //condição para qundo o scroll for maior ou igual a altura do header
-    header.classList.add('scroll')
-  } else {
-    //condição para qundo o scroll for menor que a altura do header
-    header.classList.remove('scroll')
-  }
-})
-
-// Testimonials carrousel slider swiper
-const swiper = new Swiper('.swiper', {
-  slidesPerView: 1,
-  pagination: {
-    el: '.swiper-pagination'
-  },
-  mousewheel: false,
-  keyboard: true,
-  breakpoints: {
-    767: {
-      slidesPerView: 2,
-      setWrapperSize: true
-    }
-  }
-})
-
-//ScrollReveal
-window.sr = ScrollReveal({
-  reset: true,
-  duration: 700,
-  origin: 'top',
-  distance: '30px'
-})
-
-sr.reveal(
-  '#home .image, #home .text, #about .image, #about .text, #services header, #services .card, #testimonials header, #testimonials .testimonials, #contact .text, #contact .links, footer .brand, footer .social',
-  { interval: 100 }
-)
-
-//Back to top
-const backToTop = document.querySelector('.back-to-top')
-window.addEventListener('scroll', function () {
-  if (window.scrollY >= 560) {
-    backToTop.classList.add('show')
-  } else {
-    backToTop.classList.remove('show')
-  }
-})
-
-//Activate menu at current section
-const sections = document.querySelectorAll('main section[id]')
-
-window.addEventListener('scroll', function () {
-  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
-
-  for (const section of sections) {
-    const sectionTop = section.offsetTop
-    const sectionHeight = section.offsetHeight
-    const sectionId = section.getAttribute('id')
-
-    const checkpointStart = checkpoint >= sectionTop
-    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
-
-    if (checkpointStart && checkpointEnd) {
-      document
-        .querySelector('nav ul li a[href*=' + sectionId + ']')
-        .classList.add('active')
-    } else {
-      document
-        .querySelector('nav ul li a[href*=' + sectionId + ']')
-        .classList.remove('active')
-    }
-  }
+        const pokemonListSelected = document.getElementById(pokemonSelectID)
+        pokemonListSelected.classList.add('select')
+    })
 })
